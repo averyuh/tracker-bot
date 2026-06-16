12e703fc495a1907d97c33a110c143008fab7cf2
ave <68245869+knownasivy@users.noreply.github.com>
2026-06-16 16:37:12 -0400

    Restructure the models of the file and add file blob struct.

--- website/migrations/20260616082435_create_file_table.sql
+++ website/migrations/20260616082435_create_file_table.sql
@@ -1,4 +1,31 @@
+CREATE TABLE file_blobs (
+    id UUID PRIMARY KEY,
+    storage_path TEXT NOT NULL,
+
+    hash BYTEA NOT NULL,
+    size BIGINT NOT NULL,
+
+    file_type TEXT
+);
+
+-- dedup lookup
+CREATE UNIQUE INDEX idx_file_blobs_hash
+ON file_blobs(hash);
+
 CREATE TABLE files (
     id UUID PRIMARY KEY,
-    name TEXT NOT NULL
+
+    upload_id TEXT NOT NULL UNIQUE,
+
+    blob_id UUID NOT NULL REFERENCES file_blobs(id) ON DELETE RESTRICT,
+
+    original_name TEXT NOT NULL,
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
+
+CREATE INDEX idx_files_blob_id
+ON files(blob_id);
+
+CREATE INDEX idx_files_upload_id
+ON files(upload_id);
--- website/src/uploads/models.rs
+++ website/src/uploads/models.rs
@@ -1,27 +1,60 @@
+use std::path::Path;
+
+use nanoid::nanoid;
+use time::OffsetDateTime;
 use uuid::Uuid;
 
+#[derive(Debug, Clone)]
+pub struct FileBlob {
+    pub id: Uuid,
+    pub file_path: String,
+    pub hash: [u8; 32],
+    pub size: i64,
+    /* TODO: Store file format
+        make sure file format matches file extension of File.
+        File format will be detected by magic string.
+    */ 
+}
+
 // TODO: Remove default
-#[derive(Clone, serde::Serialize)]
+#[derive(Debug, Clone)]
 pub struct File {
     pub id: Uuid,
-    pub name: String,
-    // file path?
-    // create date?
-    // share string?
-    // type?
+    pub upload_id: String, // Sharable id
+    pub blob_id: Uuid,
+    pub original_name: String,
+    pub created_at: OffsetDateTime,
+}
+
+impl File {
+    pub fn new(original_name: String, blob_id: Uuid) -> Self {
+        Self {
+            id: Uuid::now_v7(),
+            upload_id: nanoid!(8), 
+            blob_id,
+            original_name,
+            created_at: OffsetDateTime::now_utc(),
+        }
+    }
+
+    pub fn extension(&self) -> Option<&str> {
+        Path::new(&self.original_name)
+            .extension()
+            .and_then(|e| e.to_str())
+    }
+}
+
+#[derive(Debug, Clone, serde::Serialize)]
+pub struct FileResponse {
+    pub id: Uuid,
+    pub file_name: String,
 }
 
 impl From<File> for FileResponse {
     fn from(file: File) -> Self {
         Self {
             id: file.id,
-            name: file.name,
+            file_name: file.original_name,
         }
     }
-}
-
-#[derive(Clone, serde::Serialize, Default)]
-pub struct FileResponse {
-    pub id: Uuid,
-    pub name: String,
 }
